a75280e629b7630f83b5e1a448e6a12e68acae0e
atletikpenguen <atletikpenguen@gmail.com>
2025-08-01 19:05:28 +0300

    webapp

--- src/App.tsx
+++ src/App.tsx
@@ -153,7 +153,7 @@ export default function LandingPage() {
       };
 
       // Google Apps Script Web App URL (değiştirin!)
-      const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec';
+      const GOOGLE_SCRIPT_URL = 'https://script.google.com/a/macros/analistligi.com/s/AKfycbwAjVKffZ9pQ-Ey6vNCBhV8S6_oYbp_tQNE6CInnqc_-mjxU3v5sc6mEBh1TLConLCOJQ/exec';
       
       // Send to Google Sheets
       const formData = new FormData();
